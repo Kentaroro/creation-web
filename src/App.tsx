b7635f756426c247d3906a92df6cc5438ca5062a
kentaro <kentarou.suzuki.0829@gmail.com>
2026-06-24 17:55:30 +0900

    ルーターの設定をベースに合わせた

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,23 @@ import WebSite from "./pages/WebSite";
 import Plan from "./pages/Plan";
 import Contact from "./pages/Contact";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{ index: true, element: <Home /> },
-			{ path: "landingpage", element: <LandingPage /> },
-			{ path: "website", element: <WebSite /> },
-			{ path: "plan", element: <Plan /> },
-			{ path: "contact", element: <Contact /> },
-			{ path: "*", element: <div></div> },
-		],
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <Layout />,
+			children: [
+				{ index: true, element: <Home /> },
+				{ path: "landingpage", element: <LandingPage /> },
+				{ path: "website", element: <WebSite /> },
+				{ path: "plan", element: <Plan /> },
+				{ path: "contact", element: <Contact /> },
+				{ path: "*", element: <div></div> },
+			],
+		},
+	],
+	{ basename: "/creation-web" },
+);
 
 function App() {
 	return <RouterProvider router={router} />;
